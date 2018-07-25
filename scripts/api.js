5b6a4af19fd1f55c4a74dd624704d7e82faa6523
Selvin Norman <selvin715@gmail.com>
2018-07-25 09:58:00 -0700

    working on createItem

--- scripts/api.js
+++ scripts/api.js
@@ -4,11 +4,28 @@
 
 const api=(function() {
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/selvin-jacob';
+  const createItem = function(name, callback) {
+    const newItem = {
+      name: name
+    };
+    JSON.stringify(newItem);
+    // With ajax()
+    $.ajax({
+      url: `${BASE_URL}/items`,
+      method: 'GET',
+      dataType: 'json',
+      data: { part: 'snippet' },
+      success: (data) => {
+        console.log(data)
+      }
+    });
+  }
 
-  // const getItems = function(callback) {
-  //   $.getJSON(BASE_URL + '/items', callback);
-  // };
+  const getItems = function(callback) {
+    $.getJSON(`${BASE_URL}/items`, callback);
+  };
   return {
-    // getItems,
+    //createItem,
+    getItems,
   };
 }());
