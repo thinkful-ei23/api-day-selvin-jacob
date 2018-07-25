702863715606d6ffc1c59373b98f86273540a285
Selvin Norman <selvin715@gmail.com>
2018-07-25 12:56:40 -0700

    starting to simplify store update methods

--- scripts/store.js
+++ scripts/store.js
@@ -11,23 +11,8 @@ const store = (function(){
     return this.items.find(item => item.id === id);
   };
 
-  const findAndToggleChecked = function(id) {
-    const item = this.findById(id);
-    item.checked = !item.checked;
-  };
-
-  const findAndDelete = function(id) {
+    const findAndDelete = function(id) {
     this.items = this.items.filter(item => item.id !== id);
-  };
-
-  const findAndUpdateName = function(id, name) {
-    try {
-      Item.validateName(name);
-      const item = this.findById(id);
-      item.name = name;
-    } catch(e) {
-      console.log('Cannot update name: ' + e.message);
-    }
   };
 
   const toggleCheckedFilter = function() {
@@ -38,6 +23,9 @@ const store = (function(){
     this.searchTerm = term;
   };
 
+  const findAndUpdate = function(id, newData) {
+    this.items = 
+  }
   return {
     items: [],
     hideCheckedItems: false,
@@ -45,9 +33,8 @@ const store = (function(){
 
     addItem,
     findById,
-    findAndToggleChecked,
-    findAndDelete,
-    findAndUpdateName,
+    findAndUpdate,
+    findAndDelete,    
     toggleCheckedFilter,
     setSearchTerm,
   };
